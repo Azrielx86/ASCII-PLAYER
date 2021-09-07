3067c1baf82f805839cfe2c2c112c95d53e0d319
BuildTools <unconfigured@null.spigotmc.org>
2021-09-07 02:25:26 -0500

    master

--- AsciiPlayer.py
+++ AsciiPlayer.py
@@ -1,17 +1,13 @@
-from time import time
+from time import sleep, time
 import moviepy.editor as mp
 import sys
-from os import name, system
+from os import name, system, path
 import cv2 as cv
 from PIL import Image
 import progressbar
 import fpstimer
-import playsound
-
-if name == 'nt':
-    CLEAR = 'cls'
-else:
-    CLEAR = 'clear'
+from pygame import mixer
+from threading import Thread
 
 class AsciiPlayer:
     def __init__(self, path) -> None:
@@ -58,18 +54,13 @@ class AsciiPlayer:
             if success:
                 if self.verificar_archivos(f'./files/output/frame_{nFrame}.jpg') == False:
                     cv.imwrite(f'./files/output/frame_{nFrame}.jpg', frame)
-                else:
-                    #print('Archivo encontrado, no se sobreescribio')
-                    pass
             else:
                 break
             nFrame += 1
 
-            """ if nFrame == 100:
-                break """
-
             progreso.update(nFrame)
 
+        progreso.finish()
         capturas.release()
 
     def obtener_ascii(self):
@@ -77,7 +68,6 @@ class AsciiPlayer:
         ASCII_CHARS = [' ', ':', '!', '*', '%', '$', 'S', 'O', '&', '#', '@']
 
         for i in range(self.contar_frames()):
-        #for i in range(100):
             try:
                 frame = Image.open('./files/output/frame_%s.jpg'%i)
                 frame = frame.resize(size=[self._width, self._height])
@@ -103,7 +93,7 @@ class AsciiPlayer:
             except Exception as e:
                 sys.stdout.write(f'Ocurrio un error al formar la imagen ASCII: {e}')
 
-    def guardar_ascii_temporal(self):
+    def recuperar_txt(self):
         self._ascii_frames = [[]*k for k in range(self.contar_frames())]
         try:
             with open('./files/frames.txt', 'r') as frames:
@@ -114,11 +104,9 @@ class AsciiPlayer:
             sys.stdout.write(f'Ocurrio un error al abrir el archivo: {e}')
 
     def reproducir_cancion(self):
-        try:
-            with playsound('./files/bad_apple.mp3') as song:
-                song
-        except Exception as e:
-            sys.stdout.write(f'Ocurrio un error al abrir el archivo de audio: {e}')
+        mixer.init()
+        mixer.music.load("./files/bad_apple.mp3")
+        mixer.music.play()
 
     def reproducir_frames(self):
         system('mode %s, %s'%(self._width, self._height))
@@ -127,13 +115,26 @@ class AsciiPlayer:
             sys.stdout.write(self._ascii_frames[i])
             timer.sleep()
 
+    def player(self):
+        audio = Thread(target=self.reproducir_cancion())
+        txt = Thread(target=self.reproducir_frames())
+
+        audio.start()
+        txt.start()
+
+        txt.join()
+        audio.join()
+        
+
 if __name__ == '__main__':
     ruta = str('./bad_apple.mp4')
     BadApple = AsciiPlayer(ruta)
 
-    #BadApple.convertir_audio()
+    #BadApple.reproducir_cancion()
+    BadApple.convertir_audio()
     #BadApple.obtener_frames()
     #BadApple.obtener_ascii()
-    BadApple.guardar_ascii_temporal()
-    BadApple.reproducir_frames()
+    BadApple.recuperar_txt()
+    #BadApple.reproducir_frames()
     
+    BadApple.player()
