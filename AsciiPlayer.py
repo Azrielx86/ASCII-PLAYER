f63e3f5f51bf28b88182cba15e7e53ccd254cecf
BuildTools <unconfigured@null.spigotmc.org>
2021-09-07 10:11:50 -0500

    master

--- AsciiPlayer.py
+++ AsciiPlayer.py
@@ -1,4 +1,4 @@
-from os import name, system, path
+from os import name, system, path, remove
 from PIL import Image
 from pygame import mixer
 from threading import Thread
@@ -7,6 +7,7 @@ import cv2 as cv
 import progressbar
 import fpstimer
 import sys
+import playsound
 
 class AsciiPlayer:
     def __init__(self, path) -> None:
@@ -32,10 +33,13 @@ class AsciiPlayer:
 
     @classmethod
     def preparar_directorios(cls):
-        if name == 'nt':
+        """ if name == 'nt':
             system('mkdir .\\files\\output')
         else:
-            system('mkdir ./files/output')
+            system('mkdir ./files/output') """
+
+        system('mkdir ' + path.join('files', 'output'))
+
 
     def verificar_archivos(self, ruta: str) -> bool:
         try:
@@ -45,11 +49,11 @@ class AsciiPlayer:
             return False
 
     def convertir_audio(self) -> None:
-        if self.verificar_archivos('./files/bad_apple.wav'):
+        if self.verificar_archivos(path.join("files", "audio.wav")):
             sys.stdout.write('El archivo de audio ya existe')
         else:
             audio = mp.VideoFileClip(self._path)
-            audio.audio.write_audiofile('./files/bad_apple.wav', bitrate='320k')
+            audio.audio.write_audiofile(path.join("files", "audio.wav"), bitrate='320k')
 
     def contar_frames(self) -> int:
         video = cv.VideoCapture(self._path)
@@ -121,10 +125,21 @@ class AsciiPlayer:
         except Exception as e:
             sys.stdout.write(f'Ocurrio un error al abrir el archivo: {e}')
 
+    def preparar_archivos(self):
+        self.convertir_audio()
+        if self.verificar_archivos(path.join("files", "frames.txt")):
+            self.recuperar_txt()
+        else:
+            self.obtener_frames()
+            self.obtener_ascii()
+
+    def eliminar_archivos(self):
+        remove(path.join('files'))
+
     def reproducir_cancion(self) -> None:
         mixer.pre_init(frequency=44100,size= -16,channels= 2,buffer= 2048)
         mixer.init()
-        mixer.music.load(path.join("files", "bad_apple.wav"))
+        mixer.music.load(path.join("files", "audio.wav"))
         mixer.music.play()
 
     def reproducir_frames(self) -> None:
@@ -143,16 +158,60 @@ class AsciiPlayer:
 
         txt.join()
         audio.join()
+
+def main():
+    system('cls' if name == 'nt' else 'clear')
+    while True:
+        print('ASCII Video Player'.center(50, '='))
+        ruta = input("Ingresa la ruta del archivo (Preferiblemente en root):").strip()
+        try:
+            with open(ruta, 'r'):
+                video = AsciiPlayer(ruta)
+                break
+        except Exception as e:
+            print(f'Ocurrio un error: {e}')
+            input()
+    
+    while True:
+        print('[1] Preparar archivos')
+        print('[2] Reproducir')
+        print('[3] Eliminar archivos')
+        print('[4] Salir')
+        opcion = input('>')
+
+        if opcion == '1':
+            print('Ingresa la ruta del archivo: ')
+            video.preparar_archivos()
+
+        elif opcion == '2':
+            video.recuperar_txt()
+            video.player()
+
+        elif opcion == '3':
+            print('¿Eliminar archivos? [y/n]')
+            opcion = input()
+            if opcion == 'y' or opcion == 'Y':
+                pass
+            elif opcion == 'n' or opcion == 'N':
+                pass
+            else:
+                print('No valido')
+        elif opcion == '4':
+            break
+        else:
+            print('Opcion invalida!')
         
 
 if __name__ == '__main__':
+    main()
     #ruta = str('./bad_apple.mp4')
-    ruta = str('./lagtrain.mp4')
-    BadApple = AsciiPlayer(ruta)
+    #ruta = str('./lagtrain.mp4')
+    #BadApple = AsciiPlayer(ruta)
+    #BadApple.width = 220
 
-    BadApple.convertir_audio()
-    BadApple.obtener_frames()
-    BadApple.obtener_ascii()
-    BadApple.recuperar_txt()
+    #BadApple.convertir_audio()
+    #BadApple.obtener_frames()
+    #BadApple.obtener_ascii()
+    #BadApple.recuperar_txt()
     
-    BadApple.player()
+    #BadApple.player()
